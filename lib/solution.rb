cfaa2f8918acd095de9f5dd1eb39c7007947b168
Malcolm Keyes <3.malcolmk@gmail.com>
2022-06-01 15:37:20 -0400

    commit tree c8c0bf897d2237a3859520c40937740dd0500ff3

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,22 @@
 def clock_angle(time)
     # code your solution here
+    time_array = time.split(":")
+    hour = time_array[0].to_f
+    minute = time_array[1].to_f
+
+    # Hour hand
+    # 12 = 0 degrees
+    # 3 = 90 degrees
+    # 6 = 180 degrees
+    # 9 = 270 degrees
+
+    hour == 12 ? hour = 0 : nil
+
+    degrees_per_hour = 360 / 12
+    degrees_per_minute = 360 / 60
+
+    minute_position = minute * degrees_per_minute
+    hour_position = hour * degrees_per_hour + (degrees_per_hour * minute / 60)
+
+    hour_position >= minute_position ? hour_position - minute_position :  360 - (minute_position - hour_position)
 end
